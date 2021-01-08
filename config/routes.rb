fe7af9b06352e512722c18df282a25126a155481
takumifunayama <mt.koala01n@gmail.com>
2021-01-08 14:26:15 +0900

    [Add]deviseによるログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'top#top'
 
+  get 'signup', to: 'users#new'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
   resources :users, only: %i[new create index show destroy]
+  resources :items, only: %i[new create index show destroy]
 end
